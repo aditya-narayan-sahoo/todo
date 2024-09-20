ac160617bad97f6c0e7bbf7d5ec3fd70d0618f8f
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-21 00:05:02 +0530

    Refactor code

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -7,23 +7,23 @@ export default function TodoForm() {
 
   const addItem = (e) => {
     e.preventDefault();
-    if (todo) return;
+    if (!todo) return;
     addTodo({ todo: todo, completed: false });
     setTodo("");
   };
 
   return (
-    <form className="flex" onSubmit={addItem}>
+    <form className="flex items-center mb-6" onSubmit={addItem}>
       <input
         type="text"
-        placeholder="Enter your Todo."
-        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        placeholder="Enter your Todo"
+        className="w-full rounded-l-lg px-4 py-2 outline-none focus:border-blue-500 duration-200 bg-white/10 hover:bg-white/30"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
       <button
         type="submit"
-        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+        className="rounded-r-lg px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 transition duration-200 shrink-0"
       >
         Add
       </button>
